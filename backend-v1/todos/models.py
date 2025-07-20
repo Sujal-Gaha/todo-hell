096b97b1b029aaaf68c840c6085b89b23e6be01f
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-07-20 12:34:23 +0545

    added priority enum for todo

--- backend-v1/todos/models.py
+++ backend-v1/todos/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Todo(models.Model):
+    
+    class Priority(models.TextChoices):
+        HIGH = "HI", _('HIGH')
+        LOW = "LO", _("LOW")
+        MEDIUM = "ME", _("MEDIUM")
+    
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
+    priority = models.CharField(
+        max_length = 2,
+        choices = Priority.choices,
+        default = Priority.LOW,
+    )
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
