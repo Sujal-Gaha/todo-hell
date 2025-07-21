d84bdf23a657cc7c62280d8428901061dafb1e06
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-07-21 10:29:31 +0545

    redefined the modal for todo

--- backend-v1/todos/models.py
+++ backend-v1/todos/models.py
@@ -7,17 +7,21 @@ class Todo(models.Model):
         HIGH = "HI", _('HIGH')
         LOW = "LO", _("LOW")
         MEDIUM = "ME", _("MEDIUM")
+        URGENT = "UR", _("URGENT")
     
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.CharField(max_length=200, blank=True, default="")
     priority = models.CharField(
         max_length = 2,
         choices = Priority.choices,
         default = Priority.LOW,
     )
     completed = models.BooleanField(default=False)
+    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} - {self.priority}"
