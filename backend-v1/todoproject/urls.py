d2c929405fe01c9e5ee362d1f196589d7964c997
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-07-21 11:24:19 +0545

    configured the base api path for v1 backend

--- backend-v1/todoproject/urls.py
+++ backend-v1/todoproject/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+BASE_API_PATH = 'api/v1/'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('todos.urls')),
+    path(BASE_API_PATH, include('todos.urls')),
 ]
--- backend-v1/todos/serializers.py
+++ backend-v1/todos/serializers.py
@@ -4,4 +4,4 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'completed', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'description', 'category', 'priority', 'completed', 'created_at', 'updated_at', 'due_date']
